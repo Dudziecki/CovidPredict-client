15df9300ded692cfb6a1bcd382237ce1292d3632
Michael Dudzik <mkdudik15@gmail.com>
2025-03-26 12:49:17 +0300

    adding export jackson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.example.client {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires com.fasterxml.jackson.annotation;
 
     opens com.example.client to javafx.fxml;
     exports com.example.client;
