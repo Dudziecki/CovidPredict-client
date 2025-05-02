be3a7f466d158aab64b64b0a6b1acbb89b262cb2
Michael Dudzik <mkdudik15@gmail.com>
2025-05-02 20:09:24 +0300

    add class for region ranking

---
+++ src/main/java/com/example/client/model/RegionRanking.java
@@ -0,0 +1,11 @@
+package com.example.client.model;
+
+public class RegionRanking {
+    private String region;
+    private int totalInfected;
+
+    public String getRegion() { return region; }
+    public void setRegion(String region) { this.region = region; }
+    public int getTotalInfected() { return totalInfected; }
+    public void setTotalInfected(int totalInfected) { this.totalInfected = totalInfected; }
+}
